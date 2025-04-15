0935e361826959a52bf203cc18347674852c3a4c
CM1435 <chandrasekharmehta9@gmail.com>
2025-04-15 15:34:12 +0530

    issue resolved

--- codeFile.c
+++ codeFile.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(){
     printf("changed is made in code explicitly");
+    printf("issue resolved");
     return 0;
 }
--- fileInNew.c
+++ fileInNew.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(){
+int main() {
     printf("File in New Branch");
     return 0;
 }
